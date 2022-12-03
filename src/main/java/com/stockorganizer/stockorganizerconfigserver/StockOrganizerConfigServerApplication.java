18723b8959c064ea1b38f25ebc124c77e563189d
AnnaTveritnyeva <atveretniv@gmail.com>
2022-12-03 18:08:52 +0200

    Initial commit

---
+++ src/main/java/com/stockorganizer/stockorganizerconfigserver/StockOrganizerConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.stockorganizer.stockorganizerconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOrganizerConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOrganizerConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockorganizer/stockorganizerconfigserver/StockOrganizerConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockorganizer.stockorganizerconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrganizerConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
